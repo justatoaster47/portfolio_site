ab72194abb203dc6b502c9b518c20d50e456bb86
Alexander Petro <alexmpetro@gmail.com>
2023-07-30 11:42:17 -0700

    working hover functionality for nav

--- components/Nav.js
+++ components/Nav.js
@@ -7,8 +7,16 @@ const Nav = () => {
 
   const [isVisible, setIsVisible] = useState(false);
 
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible);
+  // const toggleVisibility = () => {
+  //   setIsVisible(!isVisible);
+  // };
+
+  const handleMouseEnter = () => {
+    setIsVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsVisible(false);
   };
 
 
@@ -17,28 +25,31 @@ const Nav = () => {
 
       {isVisible ? ( 
         <>
-        <nav className={'flex justify-around h-full w-full      transition-all duration-300 ease-in ${isVisible ? "top-0" : "top-[-40]"}'}>
+        <nav className={'flex justify-around h-full w-1/2 transition-all duration-300 ease-in' + (isVisible ? "h-45" : "h-20]")}>
         
           <a href="/#" className='hover:text-gray-400 transform '>Home</a>
           <a href="/#" className='hover:text-gray-400 transform '>My work</a>
           <a href="/#" className='hover:text-gray-400 transform '>Blog</a>
           <a href="/#" className='hover:text-gray-400 transform '>About me</a>
         </nav> 
-        <button className=' hover:text-gray-400 bg-transparent border-none outline-none transform hover:opacity-70' onClick={toggleVisibility}>
-          <FaTimes />
-        </button>
+        
         </>
       ) : (
-        <nav className='flex justify-between items-center w-full h-full       '>
+        <nav className='flex justify-between items-center w-1/2 h-full '>
           <h3>LOGO</h3>
 
           <h1>Title</h1>
 
-          <button className=' hover:text-gray-400 bg-transparent border-none outline-none transform' onClick={toggleVisibility}>
-            <FaBars />
-          </button>
         </nav>
       )}
+
+      <button 
+        className='z-50 hover:text-gray-400 duration-150 hover:rotate-180 bg-transparent border-none outline-none transform' 
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <FaBars />
+      </button>
 
 		</header>
   );
