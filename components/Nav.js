3567bc560191cae05f049cacdf7e218549a34b0e
Alexander Petro <alexmpetro@gmail.com>
2023-07-30 12:53:12 -0700

    Faded nav bar

--- components/Nav.js
+++ components/Nav.js
@@ -21,32 +21,30 @@ const Nav = () => {
 
 
   return (
-    <header className='flex items-center justify-between h-30 p-5 bg-slate-200'>
+    <header>
+    
+      <h3 className='z-50 absolute left-5 top-5'>LOGO</h3>
 
-      {isVisible ? ( 
-        <>
-        <nav className={'flex justify-around h-full w-1/2 transition-all duration-300 ease-in' + (isVisible ? "h-45" : "h-20]")}>
-        
-          <a href="/#" className='hover:text-gray-400 transform '>Home</a>
-          <a href="/#" className='hover:text-gray-400 transform '>My work</a>
-          <a href="/#" className='hover:text-gray-400 transform '>Blog</a>
-          <a href="/#" className='hover:text-gray-400 transform '>About me</a>
-        </nav> 
-        
-        </>
-      ) : (
-        <nav className='flex justify-between items-center w-1/2 h-full '>
-          <h3>LOGO</h3>
-
-          <h1>Title</h1>
-
-        </nav>
-      )}
+      <nav
+      className={`flex items-center justify-center gap-10 h-30 p-5 bg-slate-200 duration-300 transition-opacity ${
+        isVisible ? 'opacity-100' : 'opacity-0'
+      }`}
+      onMouseLeave={handleMouseLeave}
+      >
+        <a href="/#" className="hover:text-gray-400 transform">
+          Projects
+        </a>
+        <a href="/#" className="hover:text-gray-400 transform">
+          Resume
+        </a>
+        <a href="/#" className="hover:text-gray-400 transform">
+          Contact
+        </a>
+      </nav>
 
       <button 
-        className='z-50 hover:text-gray-400 duration-150 hover:rotate-180 bg-transparent border-none outline-none transform' 
+        className='absolute bg-transparent border-none outline-none right-5 top-5 text-xl ' 
         onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
       >
         <FaBars />
       </button>
