f24b4d2aa78529b883bce9c8fd0a0db6dfdc87a0
Alexander Petro <alexmpetro@gmail.com>
2023-07-30 17:09:16 -0700

    Fully made nav, just need links

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,9 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import dynamic from 'next/dynamic'; // This fixes hydration issues.
+import Image from 'next/image';
 
 const Nav = () => {
 
@@ -15,31 +17,38 @@ const Nav = () => {
     setIsVisible(false);
   };
 
+  useEffect(() => {
+    // Use useEffect only for client-side logic
+    const isClient = typeof window !== 'undefined';
+    if (isClient) {
+      // Use window or other client-side APIs here, if necessary
+    }
+  }, []);
+
 
   return (
     <header>
     
-      <h3 className='z-50 absolute left-5 top-5'>LOGO</h3>
+      <a href="/"> 
+        <Image src='/react-icon.png' alt='Logo' height={65} width={65} className='z-10 absolute p-5 '/>
+      </a>
+
 
       <nav
-        className={`flex items-center justify-center gap-10 h-30 p-5 bg-gradient-to-b from-gray-500 to-white duration-300 transition-opacity ${
-          isVisible ? 'opacity-100' : 'opacity-0'
-        }`}
+        className={`flex items-center justify-center gap-10 h-30 p-5 bg-gradient-to-b from-gray-500 to-white duration-500 transition-opacity ${ isVisible ? "opacity-100" : "opacity-0"}`}
         onMouseLeave={handleMouseLeave}
       >
-        <a href="/#" className="hover:text-gray-400 transform">
+
+        <a href="/#" className="hover:text-gray-600 transform">
           Projects
         </a>
-        <a href="/#" className="hover:text-gray-400 transform">
+        <a href="/#" className="hover:text-gray-600 transform">
           Resume
         </a>
-        <a href="/#" className="hover:text-gray-400 transform">
+        <a href="/#" className="hover:text-gray-600 transform">
           Contact
         </a>
       </nav>
-
-
-     
 
       <button 
         className='absolute bg-transparent border-none outline-none right-5 top-5 text-xl ' 
@@ -55,4 +64,5 @@ const Nav = () => {
 
 
 
-export default Nav
+// export default Nav
+export default dynamic(() => Promise.resolve(Nav), {ssr: false}); //this fixes hydration issues
